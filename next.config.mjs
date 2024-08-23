3296616868e6f24cd98ef03e0b6904f5bbfc3661
sjd <abccnn09@gmail.com>
2024-08-23 11:32:42 +0330

    adding  action.yml and publish.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/quiz-app-NEXT",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
